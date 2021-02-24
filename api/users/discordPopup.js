d200e3ceac023bc201d8a752a574df89a999f50e
OscarNOW <56079903+OscarNOW@users.noreply.github.com>
2021-02-24 14:47:49 +0100

    Delete api directory

--- api/users/discordPopup.js
+++
@@ -1,8 +0,0 @@
-//users/discordPopup
-module.exports = {
-    execute(error, end, req, extra, params) {
-
-        end(JSON.stringify(extra.accountsNotOnDiscord))
-
-    }
-}
--- api/users/roles.js
+++
@@ -1,6 +0,0 @@
-//users/roles
-module.exports = {
-    execute(error, end, req, extra, params) {
-        return end(JSON.stringify(extra.roles))
-    }
-}

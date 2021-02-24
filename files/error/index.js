67dadbd2d9d9b1548dfbcc06b7635acb549eb21c
OscarNOW <56079903+OscarNOW@users.noreply.github.com>
2021-02-24 14:47:40 +0100

    Delete files directory

--- files/error/index.js
+++
@@ -1,4 +0,0 @@
-function onload() {
-    document.getElementById('title').innerText = document.getElementById('errorMessage').innerText
-    if (document.getElementById('errorFile').innerText == '|errorFile|') document.getElementById('errorFileWrapper').parentElement.removeChild(document.getElementById('errorFileWrapper'))
-}
